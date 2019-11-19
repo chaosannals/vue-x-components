bd807c930d2c868cbc583a962439b757a6462e81
chaosannals <chaosannals@outlook.com>
2019-11-19 17:51:50 +0800

    扩展路由。

--- source/router.js
+++ source/router.js
@@ -12,9 +12,28 @@ export default class XRouter extends VueRouter {
 
     /**
      * 重置路由。
+     * 
      */
     reset(options) {
         const other = new VueRouter(options || this.options);
         this.matcher = other.matcher;
+    }
+
+    /**
+     * 捕获同个路由导致的异常。
+     * 
+     * @param {*} location 
+     * @param {*} resolve 
+     * @param {*} reject 
+     */
+    push(location, resolve, reject) {
+        let result = super.push(location, resolve, reject);
+        if (result) {
+            result.catch(e => {
+                if (e.name != 'NavigationDuplicated') {
+                    Promise.reject(e);
+                }
+            });
+        }
     }
 }
