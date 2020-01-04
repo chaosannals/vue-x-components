d9bddc3840a09810f746a91e428157f9190783dc
chaosannals <chaosannals@outlook.com>
2020-01-04 15:27:35 +0800

    图片处理函数。

---
+++ source/image.js
@@ -0,0 +1,48 @@
+export function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        let image = new Image();
+        image.src = url;
+        image.onload = () => resolve(image);
+        image.onerror = e => reject(e);
+    });
+}
+
+export function imageToCanvas(image) {
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    return canvas;
+}
+
+export function canvasToBlob(canvas, type, quality) {
+    return new Promise(resolve => {
+        canvas.toBlob(
+            blob => resolve(blob),
+            type || 'image/jpeg',
+            quality || 1.0);
+    });
+}
+
+export function blobToDataURL(blob) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onloadend = e => resolve(e.target.result);
+        reader.onerror = e => reject(e);
+        reader.readAsDataURL(blob);
+    });
+}
+
+export function dataURLToBlob(dataURL) {
+    let info = dataURL.split(',');
+    let mime = info[0].match(/:(.*?);/)[1];
+    let text = atob(info[1]);
+    let data = new Uint8Array(text.length);
+    for (let i = 0; i < text.length; ++i) {
+        data[i] = text.charCodeAt(i);
+    }
+    return new Blob([data], {
+        type: mime
+    });
+}
